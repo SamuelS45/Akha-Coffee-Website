8a6c37f1175f5f53c4c9da2e8f2a9fdbb7b9d145
SamuelS45 <104377311+SamuelS45@users.noreply.github.com>
2022-09-28 01:26:39 +0700

    Update for small screens

--- functions.js
+++ functions.js
@@ -1,13 +1,15 @@
 
-let toTop = document.getElementById('toTop');
+let toTop = document.querySelector('#toTop');
 
 window.onscroll = function(){scrollFunction()}
 
 function scrollFunction(){
     if(document.body.scrollTop> 20||document.documentElement.scrollTop > 20){
         toTop.style.display="block"
+        // toTop.style.width="20rem"
     }else{
         toTop.style.display="none"
+        // toTop.style.width="0px"
     }
 }
 
