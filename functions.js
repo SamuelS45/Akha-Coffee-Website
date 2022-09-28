35ab5e36c54a737b237d03d518bd4c1cea227c86
SamuelS45 <104377311+SamuelS45@users.noreply.github.com>
2022-09-28 15:22:20 +0700

    fixed hamburger menu

--- functions.js
+++ functions.js
@@ -1,15 +1,13 @@
 
-let toTop = document.querySelector('#toTop');
+let toTop = document.getElementById('toTop');
 
 window.onscroll = function(){scrollFunction()}
 
 function scrollFunction(){
     if(document.body.scrollTop> 20||document.documentElement.scrollTop > 20){
         toTop.style.display="block"
-        // toTop.style.width="20rem"
     }else{
         toTop.style.display="none"
-        // toTop.style.width="0px"
     }
 }
 
@@ -17,3 +15,28 @@ toTop.onclick = function(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+let ham = document.querySelector('.hamburger')
+let hamMenu = document.querySelector('.ham-menu')
+
+let active = false
+ham.addEventListener('click', ()=>{
+    active = !active
+    if(active == true){
+
+        ham.setAttribute('class', 'hamburger-rotated')
+        // ham.setAttribute('id', 'active')
+        
+        hamMenu.style.width = '20rem'
+        // hamMenu.style.display = 'flex'
+    }else if(active == false){
+        
+        ham.setAttribute('class', 'hamburger')
+        // ham.setAttribute('id', 'not')
+        // hamMenu.style.display = 'none'
+        hamMenu.style.width = '0px'
+        
+    }
+})
+// if(ham)
+
