43f89037b8a310fc9e0a7e46a5d2e74d13679c57
SamuelS45 <104377311+SamuelS45@users.noreply.github.com>
2022-09-30 15:48:26 +0700

    Added User Page and Button

--- functions.js
+++ functions.js
@@ -16,27 +16,5 @@ toTop.onclick = function(){
     document.documentElement.scrollTop = 0;
 }
 
-let ham = document.querySelector('.hamburger')
-let hamMenu = document.querySelector('.ham-menu')
 
-let active = false
-ham.addEventListener('click', ()=>{
-    active = !active
-    if(active == true){
-
-        ham.setAttribute('class', 'hamburger-rotated')
-        // ham.setAttribute('id', 'active')
-        
-        hamMenu.style.width = '20rem'
-        // hamMenu.style.display = 'flex'
-    }else if(active == false){
-        
-        ham.setAttribute('class', 'hamburger')
-        // ham.setAttribute('id', 'not')
-        // hamMenu.style.display = 'none'
-        hamMenu.style.width = '0px'
-        
-    }
-})
-// if(ham)
 
